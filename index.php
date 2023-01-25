a29b450dae7d9cd50b3953aebd7dbb04484442ab
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-25 20:36:41 +0530

    Alert add

--- index.php
+++ index.php
@@ -20,6 +20,16 @@
 </nav>
 
 <div class="container">
+    <?php
+    if(isset($_GET['msg'])){
+        $msg = $_GET['msg'];
+        echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">'.$msg.'
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>';
+
+    }
+    ?>
+
     <a href="add_new.php" class="btn btn-secondary">Add New</a>
 
     <table class="table table-hover text-center mt-3">
