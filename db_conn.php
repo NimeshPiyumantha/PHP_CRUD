607711df32709e0b0513d353984f0bd5be71fe49
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-25 17:52:22 +0530

    Add Project

---
+++ db_conn.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost:3307";
+$username = "root";
+$password = "";
+$dbname = "php_crud";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed" . mysqli_connect_error());
+}
+echo "Connected successfully";
